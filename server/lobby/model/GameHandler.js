ed19623776d786e5b6b5e40b329ec37dad3ddb5b
TALLEC <gauthier.tallec996@gmail.com>
2018-08-24 13:41:35 +0200

    First draft of saloon handler (not tested)

---
+++ server/lobby/model/GameHandler.js
@@ -0,0 +1,29 @@
+class GameHandler{
+
+  constructor(){
+    this.listOfGameLaunchers = {};
+  }
+
+  create(gameName, playerSocket){
+    /*Create an instance of a game and wait for the players to join*/
+
+    if !(gameName in this.listOfGameLaunchers){
+      this.listOfGameLaunchers[gameName] = [];
+    }
+
+    let newGameLauncher = new GameLauncher(gameName);
+    newGameLauncher.addPlayer(playerSocket);
+    this.listOfGameLaunchers[gameName].push(newGameLauncher);
+  }
+
+  join(gameName, gameNumber, playerSocket){
+    /* Join an existing game */
+    
+    if (game in this.listOfGameLaunchers){
+      if(number < this.listOfGameLaunchers.length){
+        this.listOfGameLaunchers[gameNumber].addPlayer(playerSocket);
+      }
+    }
+  }
+
+}
